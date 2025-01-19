6a4dc564fecd83cbf4d7993f5cca65ebbcb530e9
FelipeFranke5 <frankefelipee@gmail.com>
2025-01-19 03:08:23 -0300

    misc: incluir a anotação de Transactional no UserService

--- src/main/java/com/felipefranke/jira_incidents/api/user/UserService.java
+++ src/main/java/com/felipefranke/jira_incidents/api/user/UserService.java
@@ -3,10 +3,10 @@ package com.felipefranke.jira_incidents.api.user;
 import com.felipefranke.jira_incidents.api.role.RoleNotFoundException;
 import com.felipefranke.jira_incidents.api.role.RoleRepository;
 import com.felipefranke.jira_incidents.api.token.Token;
-import com.felipefranke.jira_incidents.api.token.TokenService;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+
+import jakarta.transaction.Transactional;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,7 +17,6 @@ public class UserService {
 
     private final UserRepository repository;
     private final RoleRepository roleRepository;
-    private final TokenService tokenService;
     private final JavaMailSender mailSender;
     private final Environment environment;
 
@@ -25,32 +24,16 @@ public class UserService {
         UserRepository userRepository,
         JavaMailSender mailSender,
         RoleRepository roleRepository,
-        TokenService tokenService,
         Environment environment
     ) {
         this.repository = userRepository;
         this.mailSender = mailSender;
         this.roleRepository = roleRepository;
-        this.tokenService = tokenService;
         this.environment = environment;
     }
 
-    public void validateTokenUsage(User user) {
-        if (user.getToken().getUses() > 10) {
-            throw new IllegalArgumentException("Your credentials have expired. Please create a new pair.");
-        }
-
-        this.tokenService.incrementUses(user.getToken());
-    }
-
     public User getOneUserById(UUID id) {
-        Optional<User> user = repository.findById(id);
-
-        if (user.isEmpty()) {
-            throw new UserNotFoundException();
-        }
-
-        return user.get();
+        return repository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
     public List<User> getUserList() {
@@ -66,61 +49,69 @@ public class UserService {
     public StringBuilder onUserCreationMessage(User user) {
         String randomUUID1 = UUID.randomUUID().toString();
         String randomUUID2 = UUID.randomUUID().toString();
-
-        StringBuilder bodyBuilder = new StringBuilder()
-            .append("Hello, " + user.getName() + "!")
-            .append("\n\n")
-            .append("You just created a new User to the Jira Incidents Tracker project.")
-            .append("\n")
-            .append("Please store your credentials safely, you will need them in order to perform Authentication.")
-            .append("\n")
-            .append("You will need to encode them together in Base64.")
-            .append("\n")
-            .append("Example: '" + randomUUID1 + ":" + randomUUID2 + "'")
-            .append("\n")
-            .append(
-                "Result: '" + new UserCredentialsUtil(UUID.fromString(randomUUID1), UUID.fromString(randomUUID2)).encodeCredentials() + "'"
-            )
-            .append("\n\n")
-            .append("Your Credentials:")
-            .append("\n")
-            .append("ClientId: " + user.getClientId().toString())
-            .append("\n")
-            .append("ClientSecret: " + user.getClientSecret().toString())
-            .append("\n")
-            .append("UserId: " + user.getId());
-
-        return bodyBuilder;
+        return new StringBuilder()
+                .append("Hello, ").append(user.getName()).append("!")
+                .append("\n\n")
+                .append("You just created a new User to the Jira Incidents Tracker project.")
+                .append("\n")
+                .append("Please store your credentials safely, you will need them in order to perform Authentication.")
+                .append("\n")
+                .append("You will need to encode them together in Base64.")
+                .append("\n").append("Example: '")
+                .append(randomUUID1)
+                .append(":")
+                .append(randomUUID2)
+                .append("'")
+                .append("\n")
+                .append("Result: '")
+                .append(new UserCredentialsUtil(UUID.fromString(randomUUID1), UUID.fromString(randomUUID2)).encodeCredentials())
+                .append("'")
+                .append("\n\n")
+                .append("Your Credentials:")
+                .append("\n")
+                .append("ClientId: ")
+                .append(user.getClientId().toString())
+                .append("\n")
+                .append("ClientSecret: ")
+                .append(user.getClientSecret().toString())
+                .append("\n")
+                .append("UserId: ")
+                .append(user.getId());
     }
 
     public StringBuilder onCredentialsChangeMessage(User user) {
         String randomUUID1 = UUID.randomUUID().toString();
         String randomUUID2 = UUID.randomUUID().toString();
 
-        StringBuilder bodyBuilder = new StringBuilder()
-            .append("Hello, " + user.getName() + "!")
-            .append("\n\n")
-            .append("Your credentials have been updated.")
-            .append("\n")
-            .append("Please store your new credentials safely, you will need them in order to perform Authentication.")
-            .append("\n")
-            .append("You will need to encode them together in Base64.")
-            .append("\n")
-            .append("Example: '" + randomUUID1 + ":" + randomUUID2 + "'")
-            .append("\n")
-            .append(
-                "Result: '" + new UserCredentialsUtil(UUID.fromString(randomUUID1), UUID.fromString(randomUUID2)).encodeCredentials() + "'"
-            )
-            .append("\n\n")
-            .append("Your new Credentials:")
-            .append("\n")
-            .append("ClientId: " + user.getClientId().toString())
-            .append("\n")
-            .append("ClientSecret: " + user.getClientSecret().toString())
-            .append("\n")
-            .append("UserId: " + user.getId());
-
-        return bodyBuilder;
+        return new StringBuilder().append("Hello, ")
+                .append(user.getName()).append("!")
+                .append("\n\n")
+                .append("Your credentials have been updated.")
+                .append("\n")
+                .append("Please store your new credentials safely, you will need them in order to perform Authentication.")
+                .append("\n")
+                .append("You will need to encode them together in Base64.")
+                .append("\n")
+                .append("Example: '")
+                .append(randomUUID1)
+                .append(":")
+                .append(randomUUID2)
+                .append("'")
+                .append("\n")
+                .append("Result: '")
+                .append(new UserCredentialsUtil(UUID.fromString(randomUUID1), UUID.fromString(randomUUID2)).encodeCredentials())
+                .append("'")
+                .append("\n\n")
+                .append("Your new Credentials:")
+                .append("\n")
+                .append("ClientId: ")
+                .append(user.getClientId().toString())
+                .append("\n")
+                .append("ClientSecret: ")
+                .append(user.getClientSecret().toString())
+                .append("\n")
+                .append("UserId: ")
+                .append(user.getId());
     }
 
     public void sendMailWithCredentials(User user, String subject, StringBuilder body) {
@@ -132,10 +123,10 @@ public class UserService {
         mailSender.send(message);
     }
 
-    public User saveUser(UserRequest userRequest) {
+    @Transactional
+    public void saveUser(UserRequest userRequest) {
         User user = new User();
         UserCredentialsUtil credentialsUtil = new UserCredentialsUtil(user.getClientId(), user.getClientSecret());
-
         user.setName(userRequest.username());
         user.setPassword(credentialsUtil.encodePassword(userRequest.password()));
         user.setToken(new Token(user));
@@ -144,48 +135,39 @@ public class UserService {
         user.setActive(true);
         user.setRole(roleRepository.findByName("USER").orElseThrow(RoleNotFoundException::new));
         repository.save(user);
-
         StringBuilder emailBody = this.onUserCreationMessage(user);
         this.sendMailWithCredentials(user, "JIRA TRACKER - NEW CREDENTIALS", emailBody);
-        return user;
     }
 
+    @Transactional
     public void createNewCredentials(UUID id) {
         UUID newClientId = UUID.randomUUID();
         UUID newClientSecret = UUID.randomUUID();
+        User user = repository.findById(id).orElseThrow(UserNotFoundException::new);
 
-        Optional<User> user = repository.findById(id);
-
-        if (user.isEmpty()) {
-            throw new UserNotFoundException();
-        }
-
-        if (!user.get().isActive()) {
+        if (!user.isActive()) {
             throw new IllegalArgumentException("This user is not active.");
         }
 
-        user.get().setClientId(newClientId);
-        user.get().setClientSecret(newClientSecret);
-        user.get().setToken(new Token(user.get()));
-        repository.save(user.get());
+        user.setClientId(newClientId);
+        user.setClientSecret(newClientSecret);
+        user.setToken(new Token(user));
 
-        StringBuilder emailBody = this.onCredentialsChangeMessage(user.get());
-        this.sendMailWithCredentials(user.get(), "JIRA TRACKER - CREDENTIALS UPDATE", emailBody);
+        repository.save(user);
+        StringBuilder emailBody = this.onCredentialsChangeMessage(user);
+        this.sendMailWithCredentials(user, "JIRA TRACKER - CREDENTIALS UPDATE", emailBody);
     }
 
+    @Transactional
     public void deactivateUser(UUID id) {
-        Optional<User> user = repository.findById(id);
+        User user = repository.findById(id).orElseThrow(UserNotFoundException::new);
 
-        if (user.isEmpty()) {
-            throw new UserNotFoundException();
+        if (!user.isActive()) {
+            throw new IllegalArgumentException("This user is already inactive");
         }
 
-        if (!user.get().isActive()) {
-            throw new IllegalArgumentException("This user is already inactive.");
-        }
-
-        user.get().setActive(false);
-        repository.save(user.get());
+        user.setActive(false);
+        repository.save(user);
     }
 
     public long getUsersCount() {
@@ -193,7 +175,7 @@ public class UserService {
     }
 
     public boolean isLoginAuthenticationValid(UserLoginRequest loginRequest) {
-        User user = repository.findByName(loginRequest.username()).orElseThrow(() -> new UserNotFoundException());
+        User user = repository.findByName(loginRequest.username()).orElseThrow(UserNotFoundException::new);
         return user.isValidLogin(loginRequest.password(), loginRequest.username());
     }
 }
